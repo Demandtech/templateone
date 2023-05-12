83a6b83e374723e5298f00759e6d212922c4e5b9
Demand Btc <102271199+Demandtech@users.noreply.github.com>
2023-05-12 16:07:33 +0100

    feat:footer component

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,16 @@
+import { FaTelegramPlane, FaTwitter } from 'react-icons/fa'
 const Footer = () => {
   return (
-    <footer className='text-white p-5'>
+    <footer className='text-white p-5 flex items-center justify-between border-t-2'>
       <span>Copyright &copy; {new Date().getFullYear()} $Coins</span>
+      <div className='flex text-black gap-2'>
+        <div className='bg-white rounded-full p-2'>
+          <FaTelegramPlane className='text-2xl' />
+        </div>
+        <div className='bg-white rounded-full p-2'>
+          <FaTwitter className='text-2xl' />
+        </div>
+      </div>
     </footer>
   )
 }
